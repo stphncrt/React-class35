06601819cc577eae5ca4e80fc9ffa53b70146c7a
stphn <cretestephen@gmail.com>
2022-04-23 22:10:08 +0200

    changed css

--- week1/project/ecommerce/src/App.js
+++ week1/project/ecommerce/src/App.js
@@ -11,18 +11,19 @@ function App() {
 	useEffect(() => {
 		const filtered = products.filter((product) => selectedCategory.slice(6) === product.category);
 		setFilteredProducts(filtered);
-		console.log(selectedCategory);
 	}, [selectedCategory]);
 
 	return (
 		<div className="App">
 			<h1 id="header">Welcome to HYF store...</h1>
-			<h1 id="heading">Products</h1>
+			<h2 id="heading">Products</h2>
 			<Category
-				selectCategory={(category) => setSelectedCategory(category)}
+				selectCategory={(category) => {
+					setSelectedCategory(category);
+				}}
 				selectedCategory={selectedCategory}
 			/>
-			<Products filteredProducts={filteredProducts.length !== 0 ? filteredProducts : products} />
+			<Products filteredProducts={filteredProducts.length === 0 ? products : filteredProducts} />
 		</div>
 	);
 }
