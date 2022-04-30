9c7518abf65fcd443e54193172cd2572206efe2c
stphn <cretestephen@gmail.com>
2022-04-30 20:25:31 +0200

    Dog phote changed

--- week2/practice-exercises/1-dog-photo/dog-photo-app/src/components/DogPhoto.js
+++ week2/practice-exercises/1-dog-photo/dog-photo-app/src/components/DogPhoto.js
@@ -3,7 +3,7 @@ import React from "react";
 function DogPhoto({ dogPhoto }) {
 	return (
 		<div>
-			<img style={{ margin: "1rem", width: "20rem", height: "15rem" }} src={dogPhoto} alt="" />
+			<img style={{ margin: "1rem", width: "20rem", height: "15rem" }} src={dogPhoto} alt="Dog" />
 		</div>
 	);
 }
