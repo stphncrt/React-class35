3b83e536b04f780f8847fd5ee3edf1a8edd14217
stphn <cretestephen@gmail.com>
2022-05-10 20:09:25 +0200

    the last one

--- week3/project/ecommerce/src/pages/ProductDetailPage.js
+++ week3/project/ecommerce/src/pages/ProductDetailPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import ClipLoader from "react-spinners/ClipLoader";
